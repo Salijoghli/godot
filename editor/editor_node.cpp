ee608bfde9a853058aceae2f60a752dd3fd89f8c
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2024-10-12 19:10:18 -0400

    Add signal editor_layout_loaded to editor node

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -5653,6 +5653,7 @@ void EditorNode::_load_editor_layout() {
 		ep.step(TTR("Editor layout ready."), 5, true);
 	}
 	load_editor_layout_done = true;
+	emit_signal(SNAME("editor_layout_loaded"));
 }
 
 void EditorNode::_save_central_editor_layout_to_config(Ref<ConfigFile> p_config_file) {
@@ -7137,6 +7138,7 @@ void EditorNode::_bind_methods() {
 	ADD_SIGNAL(MethodInfo("scene_changed"));
 	ADD_SIGNAL(MethodInfo("scene_closed", PropertyInfo(Variant::STRING, "path")));
 	ADD_SIGNAL(MethodInfo("preview_locale_changed"));
+	ADD_SIGNAL(MethodInfo("editor_layout_loaded"));
 }
 
 static Node *_resource_get_edited_scene() {
