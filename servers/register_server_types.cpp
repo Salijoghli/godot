cb4b3758b5fbd9bedbba17fd3c68ca9e38fd7dc3
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-01-30 09:28:25 +0400

    Set Jolt Physics as default and remove other options

--- servers/register_server_types.cpp
+++ servers/register_server_types.cpp
@@ -329,7 +329,7 @@ void register_server_types() {
 
 	GLOBAL_DEF(PropertyInfo(Variant::STRING, PhysicsServer3DManager::setting_property_name, PROPERTY_HINT_ENUM, "DEFAULT"), "DEFAULT");
 
-	PhysicsServer3DManager::get_singleton()->register_server("Dummy", callable_mp_static(_create_dummy_physics_server_3d));
+	//PhysicsServer3DManager::get_singleton()->register_server("Dummy", callable_mp_static(_create_dummy_physics_server_3d));
 #endif // PHYSICS_3D_DISABLED
 
 #ifndef XR_DISABLED
